43eb232f19b58f1554d910bd33a53ef6c7663064
AgayevAmin <aghayevamin@gmail.com>
2023-12-09 23:32:05 +0400

    js slider1

---
+++ js/home.js
@@ -0,0 +1,35 @@
+var slides = document.querySelectorAll(".slide")
+var nextBtn = document.querySelector(".btn-next")
+var prevBtn = document.querySelector(".btn-prev")
+
+var currentIndex = 0;
+var maxNumber = slides.length - 1;
+
+NextButtonClick()
+function NextButtonClick(){
+if(currentIndex===maxNumber){
+    currentIndex=0
+}else{
+    currentIndex++
+
+}
+slides.forEach((slide, index) => {
+          slide.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+        });
+
+}
+nextBtn.addEventListener("click",NextButtonClick);
+
+function PrevButtonClick(){
+    if(currentIndex===0){
+        currentIndex=maxNumber
+    }else{
+        currentIndex--
+    
+    }
+    slides.forEach((slide, index) => {
+              slide.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+            });
+    
+    }
+    prevBtn.addEventListener("click",PrevButtonClick);
